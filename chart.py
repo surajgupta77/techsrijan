8ef43a8dbeca0d36f3affdd0d738df9a56cfe267
surajgupta77 <suraj.kumar_cs17@gla.ac.in>
2019-06-16 21:31:21 +0530

    hello suraj

---
+++ chart.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as suraj
+a=[1,2,3,4,5,6,7,]
+b=[42,24,34,50,28,17,29]
+suraj.plot(a,b,color='red',linewidth=15,linestyle='dotted' )
+suraj.xlabel("DAY")
+suraj.ylabel("TEMPERATURE")
+suraj.show()
