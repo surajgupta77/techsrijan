d14866f0b43cdcaa7abda6eabd565661ecac0f38
surajgupta77 <suraj.kumar_cs17@gla.ac.in>
2019-06-17 00:32:52 +0530

    hello suraj

--- chart.py
+++ chart.py
@@ -52,5 +52,14 @@ suraj.show()'''
 
 
 
+# py chart in circular
+
+import matplotlib.pyplot as voting
+party=["bjp","bsp","sp","tmc"]
+votes=[10,4,3,2]
+voting.axis()
+voting.pie(votes,labels=party,radius=1.5,shadow="true",explode=[0.2,0,0,0],autopct='%0.1f%%',startangle=180)
+voting.show()
+
 
 
